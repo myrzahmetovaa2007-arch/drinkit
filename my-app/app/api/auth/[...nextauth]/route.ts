a438f3cfcd27f71f8717ca11950058b755111e74
aruzzhanaa <aruzzhanaa15@gmail.com>
2025-11-28 16:23:40 +0500

    Create route.ts

---
+++ my-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+my-app/
+  app/
+    api/
+      auth/
+        [...nextauth]/
+          route.ts
+      register/
+        route.ts
+  lib/
+    mongodb.ts
+  models/
+    User.ts
+  app/register/page.tsx      (или .tsx / .jsx — как у тебя принято)
+  app/login/page.tsx
